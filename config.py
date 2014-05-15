3900e0936a89f9c05a2541ff58dade10cf6936e4
casschin <casschin@gmail.com>
2014-05-14 22:06:07 -0400

    added config

---
+++ config.py
@@ -0,0 +1,19 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'aC/jams$oN]reR*wad}g'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
